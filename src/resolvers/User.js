0587f5bd66297ab7e0f5a903176dc710f1057917
Thiago Lima <thitheguy@gmail.com>
2020-08-24 00:41:53 -0300

    Adding Books entity

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,6 +1,19 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
+    books: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, { prisma }, info) {
+            return prisma.query.books({
+                where: {
+                    reviwer: {
+                        id: parent.id
+                    }
+                }
+            })
+
+        }
+    },
     email: {
         fragment: 'fragment userId on User { id }',
         resolve(parent, args, { request }, info) {
