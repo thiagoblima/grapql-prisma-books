98d427fa5360c74f3331612c46dd33f36287acb6
Thiago Lima <thitheguy@gmail.com>
2020-08-30 00:50:41 -0300

    Adding Book Resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,12 +3,14 @@ import Query from './Query'
 import Mutation from './Mutation'
 import Subscription from './Subscription'
 import User from './User'
+import Book from './Book'
 
 const resolvers = {
     Query,
     Mutation,
     Subscription,
     User,
+    Book
 }
 
 const fragmentReplacements = extractFragmentReplacements(resolvers)
